590ae0023c4984097a2eb16584006af259ca0564
zhy <1071568810@qq.com>
2018-01-08 17:28:54 +0800

    123

--- test/echo.cpp
+++ test/echo.cpp
@@ -104,65 +104,18 @@ int test_listen_connect() {
 	return 0;
 }
 
-
-class coroutine_t {
-	friend class coroutine_pool_t;
-public:
-
-	coroutine_t() {
-		cout << "coroutine_t() {" << endl;
-	}
-
-	~coroutine_t() {
-		cout << "~coroutine_t()" << endl;
-	}
-
-	void yield() {
-		
-	}
-private:
-	static void CALLBACK FiberProc(_In_ PVOID lpParameter) {
-		cout << "void CALLBACK FiberProc(_In_ PVOID lpParameter) {"	<< endl;
-	}
-	shared_ptr<coroutine_pool_t>	_cpool;
-	LPVOID							_stack;
-	function<void(shared_ptr<coroutine_t>)>		_routine;
+int fiber(int x) {
+	if (x <= 2)
+		return 1;
+	auto subfilber = [x](coroutine_t<int>& co) {
+		co.yield(fiber(x - 1) + fiber(x - 2));
+	};
+	coroutine_t<int> co(subfilber);
+	return co.get();
 };
-
-class coroutine_pool_t {
-	friend class coroutine_t;
-public:
-	coroutine_pool_t() {
-
-	}
-
-	template<class functype>
-	void push_back(functype f) {
-
-		class test_t {
-		public:
-			~test_t() {
-				cout << "~test_t() {" << endl;
-			}
-		private:
-			int _x = 100;
-		} t;
-
-		//auto co = new_shared<coroutine_t>();
-		coroutine_t co;
-		co._routine = f;
-		co._stack = ::CreateFiberEx(0,0, FIBER_FLAG_FLOAT_SWITCH,&coroutine_t::FiberProc, nullptr);
-		SwitchToFiber(co._stack);
-	}
-private:
-
-	inline void yield(shared_ptr<coroutine_t> co) {
-		longjmp(_jb, 0);
-	}
-
-	list<shared_ptr<coroutine_t>>				_coroutines;
-	jmp_buf										_jb = { 0 };
-};
+void test_coroutine() {	
+	cout << fiber(10) << endl;
+}
 
 void test_thread_pool() {
 	auto tpool = new_shared<thread_pool_t>();
@@ -174,49 +127,13 @@ void test_thread_pool() {
 		cout << f.get() << endl;
 	}
 }
-void foo(shared_ptr<coroutine_t> co) {
-
-	class test_t2 {
-	public:
-		~test_t2() {
-			cout << "~test_t() {" << endl;
-		}
-	private:
-		int _x = 1011;
-	} t;
 
 
-	cout << "this is a coroutine " << endl;
-	co->yield();
-	cout << "this is a coroutine 2 " << endl;
-};
-void test_coroutine() {
-
-	auto cpool = new_shared<coroutine_pool_t>();
-	cpool->push_back(foo);
-}
-int bar (int aaaa, int b, int c) {
-	__asm {mov eax aaaa;
-mov ebx b
-add eax ebx
-	}
-	return aaaa;
-};
-void test_asm() {
-
-	
-
-	auto foo1 = []() {
-		return 1 + bar(1,2,3);
-	};
-
-	cout << foo1() << endl;
-}
 
 int main() {
 	//test_listen_connect();
-	//test_coroutine();
-	test_asm();
+	test_coroutine();
+	//test_asm();
 	//test_thread_pool();
 	system("pause");
 	return 0;
--- this_space.hpp
+++ this_space.hpp
@@ -1055,5 +1055,109 @@ void throw_runtimerror_impl(const char* file, int line, const char* fmt, ...) {
 		list<thread>						_threads;
 	};
 
+	namespace this_coroutine	{
+		LPVOID get_id() {
+			return ::GetCurrentFiber();
+		}
+	}
+
+
+	template<class type>
+	class coroutine_t {
+		friend class coroutine_pool_t;
+	public:
+		template<class functype>
+		coroutine_t(functype&& fn)
+			:_callee_fn(std::forward<functype>(fn)) {
+			this->_caller = ::ConvertThreadToFiberEx(this, FIBER_FLAG_FLOAT_SWITCH);
+			if (!this->_caller) {
+				this->_caller_is_callee = true;
+				this->_caller = this_coroutine::get_id();
+			}
+				
+			this->_callee = CreateFiberEx(0, 0, FIBER_FLAG_FLOAT_SWITCH, &coroutine_t::callee_proc, this);
+			::SwitchToFiber(this->_callee);
+		}
+
+		~coroutine_t() {
+			clean_callee();
+		}
+
+		operator bool() {
+			return _callee != nullptr;
+		}
+
+		type yield(const type& x) {
+			_yield_type = x;
+			switch_to_caller();
+			return move(_resume_type);
+		}
+
+		type yield(type&& x) {
+			_yield_type = move(x);
+			switch_to_caller();
+			return move(_resume_type);
+		}
+
+
+		coroutine_t<type>& resume(const type& x) {
+			_resume_type = x;
+			switch_to_callee();
+			return *this;
+		}
+
+		coroutine_t<type>& resume(type&& x) {
+			_resume_type = move(x);
+			switch_to_callee();
+			return *this;
+		}
+
+		type get() {
+			return move(_yield_type);
+		}
+
+	private:
+
+		void switch_to_callee() {
+			if (this->_callee && this->_callee != this_coroutine::get_id()) {
+				::SwitchToFiber(this->_callee);
+			}
+		}
+
+		void switch_to_caller() {
+			if(this->_caller && this->_caller != this_coroutine::get_id())
+				::SwitchToFiber(this->_caller);
+		}
+
+		void clean_callee() {
+			switch_to_caller();
+			if (!_callee_quit) {
+				_callee_quit = true;
+				if (_callee) {
+					::DeleteFiber(_callee);
+					_callee = nullptr;
+				}
+			}
+			if (!this->_caller_is_callee)
+				::ConvertFiberToThread();
+		}
+
+		static void CALLBACK callee_proc(_In_ PVOID lpParameter) {
+			auto co = (coroutine_t*)lpParameter;
+			if (this_coroutine::get_id() == co->_callee) {
+				co->_callee_fn(*co);
+				co->clean_callee();
+			}
+		}
+
+		type										_resume_type;
+		type										_yield_type;
+		bool										_caller_is_callee = false;
+		bool										_callee_quit = false;
+		LPVOID										_callee = nullptr;
+		LPVOID										_caller = nullptr;
+		function<void(coroutine_t<type>&)>			_callee_fn;
+	};
+
 }
 using namespace this_space;
