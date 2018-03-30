e34671c18df7186b58aa088ac526353477aed2ed
zhy <1071568810@qq.com>
2018-03-30 20:37:28 +0800

    ............

--- codec/codec.hpp
+++ codec/codec.hpp
@@ -2,7 +2,7 @@
 #pragma once
 
 #include "myspace/myspace_include.h"
-
+#include "myspace/memory/memory.hpp"
 myspace_begin
 
 #ifdef myspace_linux
@@ -78,6 +78,80 @@ private:
 };
 
 #endif
+
+
+class Codec
+{
+public:
+    static string encodeBase64(const string& src)
+    {
+        string result;
+
+        uint8_t a3[3] = {0}, a4[4] = {0};
+
+        size_t i = 0;
+
+        result.reserve(EncodedLength(src.size()));
+
+        for(auto c : src)
+        {
+            a3[i++] == (uint8_t)c;
+
+            if(i == 3)
+            {
+                i = 0;
+
+                a3_to_a4(a3, a4);
+
+                for(auto j = 0; j < 4; ++j)
+                {
+                    result.append(1, kBase64Alphabet[a4[j]]);
+                }
+            }
+        }
+
+        if(i != 0)
+        {
+            for(int j = i; j < 3; ++j)
+            {
+                a3[j] = 0;
+            }
+
+            a3_to_a4(a4, a3);
+
+            for(int j = 0; j < i + 1; ++j)
+            {
+                result.append(1, kBase64Alphabet[a4[j]]);
+            }
+
+            while(i++ < 3)
+            {
+                result.append(1, '=');
+            }
+        }
+
+        return move(result);
+    }
+
+private:
+
+    static size_t EncodedLength(size_t length) 
+    {
+        return (length + 2 - ((length + 2) % 3)) / 3 * 4;
+    }            
+
+    static void a3_to_a4(unsigned char * a4, unsigned char * a3) 
+    {
+        a4[0] = (a3[0] & 0xfc) >> 2;
+        a4[1] = ((a3[0] & 0x03) << 4) + ((a3[1] & 0xf0) >> 4);
+        a4[2] = ((a3[1] & 0x0f) << 2) + ((a3[2] & 0xc0) >> 6);
+        a4[3] = (a3[2] & 0x3f);
+    }   
+
+    static constexpr const char* const kBase64Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                                        "abcdefghijklmnopqrstuvwxyz"
+                                                        "0123456789+/";
+};
 
 myspace_end
 
