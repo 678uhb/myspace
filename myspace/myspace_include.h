b3b6b3a16bc6dfa06274cb436068a31ccac3dac4
zhy <1071568810@qq.com>
2018-04-06 18:54:23 +0800

    .

--- myspace/myspace_include.h
+++ myspace/myspace_include.h
@@ -88,6 +88,12 @@ using namespace chrono;
 using namespace this_thread;
 using namespace placeholders;
 
+// C++ pre-C++98: __cplusplus is 1.
+// C++98: __cplusplus is 199711L.
+// C++98 + TR1: This reads as C++98 and there is no way to check that I know of.
+// C++11: __cplusplus is 201103L.
+// C++14: __cplusplus is 201402L.
+// C++17: __cplusplus is 201703L.
 #if defined(MYSPACE_WINDOWS) && defined(MYSPACE_SHARED)
 
 #ifdef MYSPACE_EXPORTS
