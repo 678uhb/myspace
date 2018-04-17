74d762fcbc8a8505c27a04f4f1bc177ca6003605
zhy <1071568810@qq.com>
2018-04-17 14:34:23 +0800

    123

--- uuid/uuid.hpp
+++ uuid/uuid.hpp
@@ -21,28 +21,28 @@ inline std::string UUID::gen() {
   ::CoCreateGuid(&guid);
   {
     std::stringstream ss;
-    ss << std::hex << setw(sizeof(guid.Data1) << 1) << setfill('0')
+    ss << std::hex << std::setw(sizeof(guid.Data1) << 1) << std::setfill('0')
        << guid.Data1;
     uuid.append(std::move(ss.str()));
     uuid.append(1, '-');
   }
   {
     std::stringstream ss;
-    ss << std::hex << setw(sizeof(guid.Data2) << 1) << setfill('0')
+    ss << std::hex << std::setw(sizeof(guid.Data2) << 1) << std::setfill('0')
        << guid.Data2;
     uuid.append(std::move(ss.str()));
     uuid.append(1, '-');
   }
   {
     std::stringstream ss;
-    ss << std::hex << setw(sizeof(guid.Data3) << 1) << setfill('0')
+    ss << std::hex << std::setw(sizeof(guid.Data3) << 1) << std::setfill('0')
        << guid.Data3;
     uuid.append(std::move(ss.str()));
     uuid.append(1, '-');
   }
   for (auto i = 0; i < sizeof(guid.Data4); ++i) {
     std::stringstream ss;
-    ss << std::hex << setw(2) << setfill('0') << (int)guid.Data4[i];
+    ss << std::hex << std::setw(2) << std::setfill('0') << (int)guid.Data4[i];
     uuid.append(std::move(ss.str()));
   }
   return std::move(uuid);
@@ -52,7 +52,8 @@ inline std::string UUID::gen() {
   uuid_generate(uid);
   std::stringstream ss;
   for (size_t i = 0; i < sizeof(uid); ++i)
-    ss << std::hex << setw(2) << setfill('0') << (int)((char *)&uid)[0];
+    ss << std::hex << std::setw(2) << std::setfill('0')
+       << (int)((char *)&uid)[0];
   return std::move(ss.str());
 #endif
 }
