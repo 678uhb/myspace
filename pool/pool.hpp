2f37b21b112eaff16810376bfb580d95071cccd4
zhy <1071568810@qq.com>
2018-04-19 17:40:23 +0800

    Pool

--- pool/pool.hpp
+++ pool/pool.hpp
@@ -1,29 +1,13 @@
 
 #include "myspace/_/stdafx.hpp"
+#include "myspace/exception/exception.hpp"
 
 MYSPACE_BEGIN
 
 template <class X, class Creator, class Deleter> class Pool;
 
 class PoolFactory {
-  template <class X> class DefaultCreator {
-  public:
-    X *operator()() const { return new X(); }
-  };
-
 public:
-  template <class X>
-  static auto create(size_t max_count = 0)
-      -> std::shared_ptr<Pool<X, DefaultCreator<X>, std::default_delete<X>>>;
-
-  template <class X, class Creator>
-  static auto create(size_t max_count, Creator &&creator)
-      -> std::shared_ptr<Pool<X, Creator, std::default_delete<X>>>;
-
-  template <class X, class Creator>
-  static auto create(size_t max_count, const Creator &creator)
-      -> std::shared_ptr<Pool<X, Creator, std::default_delete<X>>>;
-
   template <class X, class Creator, class Deleter>
   static auto create(size_t max_count, Creator &&creator, Deleter &&deleter)
       -> std::shared_ptr<Pool<X, Creator, Deleter>>;
@@ -35,18 +19,33 @@ class Pool : public std::enable_shared_from_this<Pool<X, Creator, Deleter>> {
   typedef std::unique_ptr<X, Deleter> StorageType;
 
 public:
-  std::unique_ptr<X, std::function<void(X *)>> getUnique();
-
-  std::shared_ptr<X> getShared();
+  // these function may throw exception, due to use defined creater/deleter
+  // deleter may not to throw exception
+  // c type HANDLE(void*) , better to set X to void, which return value is
+  // shared_ptr<void>, to void trouble
+  // all the impliments like std::condition_variable
+  //
+  std::shared_ptr<X> tryGet();
+  //
+  std::shared_ptr<X> get();
+  //
+  template <class Predicate> std::shared_ptr<X> get(Predicate pred);
+  //
+  template <class Rep, class Period>
+  std::shared_ptr<X> getFor(const std::chrono::duration<Rep, Period> &timeout);
+  //
+  template <class Rep, class Period, class Predicate>
+  std::shared_ptr<X> getFor(const std::chrono::duration<Rep, Period> &timeout,
+                            Predicate pred);
 
 private:
-  StorageType _getAvaible();
+  Pool(const Pool &) = delete;
+  Pool &operator=(const Pool &) = delete;
 
   Pool(size_t max_count, Creator &&creator, Deleter &&deleter);
 
-  void put(X *px);
-
-  StorageType _createItem();
+  void put(X *x);
+  std::shared_ptr<X> tryGetUnlocked();
 
 private:
   size_t max_ = 0;
@@ -59,46 +58,87 @@ private:
 
   friend class PoolFactory;
 };
-
 template <class X, class C, class D>
-inline std::unique_ptr<X, std::function<void(X *)>> Pool<X, C, D>::getUnique() {
-  auto pool = shared_from_this();
-  std::unique_ptr<X, std::function<void(X *)>> up(
-      _getAvaible().release(), [pool](X *px) { pool->put(px); });
-  return std::move(up);
-}
-
-template <class X, class C, class D>
-inline std::shared_ptr<X> Pool<X, C, D>::getShared() {
-  auto pool = shared_from_this();
-  std::shared_ptr<X> sp(_getAvaible().release(),
-                        [pool](X *px) { pool->put(px); });
-  return std::move(sp);
-}
-
-template <class X, class C, class D>
-inline typename Pool<X, C, D>::StorageType Pool<X, C, D>::_getAvaible() {
-  Pool::StorageType x(nullptr, deleter_);
-
-  for (auto ul = std::unique_lock<std::mutex>(mtx_);;) {
-    if (!avaible_.empty()) {
-      x.reset(avaible_.front().release());
-
-      avaible_.pop_front();
-
-      occupied_++;
-
-      break;
-    } else if (max_ == 0 || avaible_.size() + occupied_ < max_) {
-      occupied_++;
-
-      return std::move(_createItem());
-    } else {
-      cond_.wait_for(ul, std::chrono::seconds(1));
-    }
+inline std::shared_ptr<X> Pool<X, C, D>::tryGetUnlocked() {
+  if (!avaible_.empty()) {
+    auto pool = shared_from_this();
+    std::shared_ptr<X> sp(avaible_.front().release(),
+                          [pool](X *x) { pool->put(x); });
+    avaible_.pop_front();
+    occupied_++;
+    return sp;
   }
 
-  return std::move(x);
+  if (max_ == 0 || avaible_.size() + occupied_ < max_) {
+    auto x = creator_();
+    if (x) {
+      occupied_++;
+      auto pool = shared_from_this();
+      return std::shared_ptr<X> sp(x, [pool](X *x) { pool->put(x); });
+    }
+  }
+  return nullptr;
+}
+
+template <class X, class C, class D>
+inline typename shared_ptr<X> Pool<X, C, D>::tryGet() {
+  auto ul = std::unique_lock<std::mutex>(mtx_);
+  return tryGetUnlocked();
+}
+
+template <class X, class C, class D>
+template <class Predicate>
+inline typename shared_ptr<X> Pool<X, C, D>::get(Predicate pred) {
+  auto ul = std::unique_lock<std::mutex>(mtx_);
+  while (!pred()) {
+    auto sp = tryGetUnlocked();
+    if (sp)
+      return sp;
+    cond_.wait(ul);
+  }
+  return tryGetUnlocked();
+}
+
+template <class X, class C, class D>
+inline typename shared_ptr<X> Pool<X, C, D>::get() {
+  auto ul = std::unique_lock<std::mutex>(mtx_);
+  auto sp = tryGetUnlocked();
+  if (sp)
+    return sp;
+  cond_.wait(ul);
+  return tryGetUnlocked();
+}
+
+template <class X, class C, class D>
+template <class Rep, class Period>
+inline typename shared_ptr<X>
+Pool<X, C, D>::getFor(const std::chrono::duration<Rep, Period> &timeout) {
+  auto ul = std::unique_lock<std::mutex>(mtx_);
+  auto sp = tryGetUnlocked();
+  if (sp)
+    return sp;
+  cond_.wait_for(timeout);
+  return tryGetUnlocked();
+}
+
+template <class X, class C, class D>
+template <class Rep, class Period, class Predicate>
+inline typename shared_ptr<X>
+Pool<X, C, D>::getFor(const std::chrono::duration<Rep, Period> &timeout,
+                      Predicate pred) {
+  auto ul = std::unique_lock<std::mutex>(mtx_);
+  auto begin_time = std::chrono::high_resolution_clock::now();
+  while (!pred()) {
+    auto sp = tryGetUnlocked();
+    if (sp)
+      return sp;
+    auto cost_time = std::chrono::high_resolution_clock::now() - begin_time;
+    if (cost_time >= timeout) {
+      break;
+    }
+    cond_.wait_for(ul, timeout - cost_time);
+  }
+  return tryGetUnlocked();
 }
 
 template <class X, class C, class D>
@@ -113,39 +153,6 @@ template <class X, class C, class D> inline void Pool<X, C, D>::put(X *px) {
   --occupied_;
   ul.unlock();
   cond_.notify_one();
-}
-
-template <class X, class C, class D>
-inline typename Pool<X, C, D>::StorageType Pool<X, C, D>::_createItem() {
-  return std::move(StorageType(creator_(), deleter_));
-}
-
-template <class X>
-inline auto PoolFactory::create(size_t max_count) -> std::shared_ptr<
-    Pool<X, PoolFactory::DefaultCreator<X>, std::default_delete<X>>> {
-  typedef Pool<X, PoolFactory::DefaultCreator<X>, std::default_delete<X>>
-      PoolType;
-
-  return std::shared_ptr<PoolType>(new PoolType(
-      max_count, PoolFactory::DefaultCreator<X>(), std::default_delete<X>()));
-}
-
-template <class X, class Creator>
-inline auto PoolFactory::create(size_t max_count, Creator &&creator)
-    -> std::shared_ptr<Pool<X, Creator, std::default_delete<X>>> {
-  typedef Pool<X, Creator, std::default_delete<X>> PoolType;
-
-  return std::shared_ptr<PoolType>(new PoolType(
-      max_count, std::forward<Creator>(creator), std::default_delete<X>()));
-}
-
-template <class X, class Creator>
-inline auto PoolFactory::create(size_t max_count, const Creator &creator)
-    -> std::shared_ptr<Pool<X, Creator, std::default_delete<X>>> {
-  typedef Pool<X, Creator, std::default_delete<X>> PoolType;
-
-  return std::shared_ptr<PoolType>(new PoolType(
-      max_count, std::move(Creator(creator)), std::default_delete<X>()));
 }
 
 template <class X, class Creator, class Deleter>
